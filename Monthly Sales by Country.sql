090c73ce831d723c850aa6f261ab4a1668e6be2b
Someshwar Baghel <105289164+Someshwar1956@users.noreply.github.com>
2025-06-06 21:39:41 -0700

    Delete Monthly Sales by Country.sql

--- Monthly Sales by Country.sql
+++
@@ -1,6 +0,0 @@
-SELECT FORMAT_DATE('%Y/%m', Date(InvoiceDate)) AS YearMonth,
-       SUM(UnitPrice*Quantity) AS Total_Sales,
-       Country
-FROM `ecommerce-dataset-462116.ecommerce.ecommerce data`
-GROUP BY YearMonth, Country
-ORDER BY YearMonth;
